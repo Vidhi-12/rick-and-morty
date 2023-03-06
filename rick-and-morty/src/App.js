3c0264f3b29a392ad9f69aefdb800e996b2c3c29
Vidhi Dhama <dhamavidhi12@gmail.com>
2023-03-07 03:26:29 +0530

    page added

---
+++ rick-and-morty/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Characters from './Pages/Characters';
+
+function App() {
+  return (
+    <div className="App">
+      <Characters />
+    </div>
+  );
+}
+
+export default App;
